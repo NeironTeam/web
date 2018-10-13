d9eb44513787a41f20fd9f0b520a9d6ab2a87b0a
vetu11 <vetu@pm.me>
2018-10-13 20:44:12 +0200

    Mouse and arrow
    Mouse and arrow with it's animation c:

---
+++ src/components/Mouse.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="mouse-and-arrow">
+            <MouseIcon/>
+            <ArrowIcon class="arrow"/>        
+        </div>
+    </div>
+    
+</template>
+
+<script>
+import MouseIcon from "./MouseIcon.vue"
+import ArrowIcon from "./ArrowIcon.vue"
+
+export default {
+    name: "Mouse",
+    components: {
+        MouseIcon,
+        ArrowIcon
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@keyframes arrow-animation {
+    0% {transform: translate(0px, -30px)}
+    20% {transform: translate(0px, -29px)}
+    50% {transform: translate(0px, -25px)}
+    80% {transform: translate(0px, -21px)}
+    100% {transform: translate(0px, -20px)}
+}
+
+.mouse-and-arrow {
+    text-align: center;
+    width: 50px;
+    display: inline-block;
+}
+.mouse {
+    display: inline-block;
+    width: 100%;
+    height: auto;
+}
+.arrow {
+    display: inline-block;
+    width: 100%;
+    height: auto;
+    top: -10px;
+    animation: linear 1s infinite alternate;
+    animation-name: arrow-animation;
+}
+</style>
---
+++ src/components/MouseIcon.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 125" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+            <g><path d="M50,86c-12.1,0-22-9.9-22-22V36c0-12.1,9.9-22,22-22s22,9.9,22,22v28C72,76.1,62.1,86,50,86z M50,18c-9.9,0-18,8.1-18,18   v28c0,9.9,8.1,18,18,18s18-8.1,18-18V36C68,26.1,59.9,18,50,18z"/></g><g><path d="M50,39c-1.1,0-2-0.9-2-2v-8c0-1.1,0.9-2,2-2s2,0.9,2,2v8C52,38.1,51.1,39,50,39z"/></g>
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MouseIcon"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
