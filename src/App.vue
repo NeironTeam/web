8c7d6557ae4ca92dfb855aa6887bb77395b68b56
Andres <andellanoalonso@gmail.com>
2018-10-13 18:37:16 +0200

    clean components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
     <div id="app">
-        <img src="./assets/logo.png">
-        <HelloWorld/>
+        <NavBar/>
     </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import NavBar from "./components/NavBar";
 
 export default {
     name: "App",
     components: {
-        HelloWorld,
+        NavBar,
     },
 };
 </script>
 
 <style>
-#app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
 </style>
