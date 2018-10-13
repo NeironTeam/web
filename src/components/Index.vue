7ac7501435a797cd80f80175565cde334ca3bdcf
Lucas Menéndez <epucas@gmail.com>
2018-10-13 21:25:09 +0200

    Home page

---
+++ src/components/Index.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="outter">
+        <div class="inner">
+            <div class="logo">
+                <Logo/>
+            </div>
+            <h1 class="title">
+                neiron
+                <small>dev</small>
+            </h1>
+            <h2 class="subtitle">Tabulamos con espacios</h2>
+        </div>
+
+        <Mouse class="mouse-icon"/>
+    </div>
+</template>
+
+<script>
+import Logo from "./Logo";
+import Mouse from "./Mouse";
+
+export default {
+    components: {
+        Logo,
+        Mouse
+    }
+}
+</script>
+
+<style>
+    .outter {
+        display: inline-block;
+        vertical-align: top;
+        width: 100vw;
+        height: 100vh;
+        position: relative;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+    }
+
+    .inner {
+        display: inline-block;
+        vertical-align: top;
+        width: 90%;
+        max-width: 400px;
+        height: auto;
+        text-align: center;
+    }
+
+    .logo {
+        display: inline-block;
+        vertical-align: top;
+        width: 60%;
+        height: auto;
+        margin: 0 auto;
+        text-align: center;
+    }
+
+    .title {
+        position: relative;
+        display: inline-block;
+        vertical-align: top;
+        text-align: center;
+        width: 100%;
+        height: auto;
+        text-align: center;
+        margin: 5vh 0;
+        font-weight: 300;
+        font-size: 6vh;
+        padding: 0;
+    }
+
+    .title small {
+        position: absolute;
+        top: 5vh;
+        font-size: 2.5vh;
+    }
+
+    .subtitle {
+        display: inline-block;
+        vertical-align: top;
+        text-align: center;
+        font-weight: 400;
+        font-size: 2.5vh;
+        color: #444;
+    }
+
+    .mouse-icon {
+        display: inline-block;
+        vertical-align: top;
+        width: 100%;
+        text-align: center;
+        margin: 3vh auto;
+        position: absolute;
+        bottom: 0;
+    }
+</style>
